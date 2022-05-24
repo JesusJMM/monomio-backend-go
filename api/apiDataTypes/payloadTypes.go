6d7ed85dcecf4aa03bec7a63a3679dea6c883ca4
JesusJMM <jesusjaviermarcanomora.2005@gmail.com>
2022-05-24 16:32:19 -0400

    define posts api types

--- api/apiDataTypes/payloadTypes.go
+++ api/apiDataTypes/payloadTypes.go
@@ -11,3 +11,9 @@ type PayloadLogin struct{
   Name string `json:"name" binding:"required"`
   Password string `json:"password" binding:"required"`
 }
+
+type PayloadPost struct{
+  Title string `json:"title" binding:"required"`
+  Description string `json:"desc" binding:"required"`
+  Content string `json:"content" binding:"required"`
+}
--- api/apiDataTypes/responseTypes.go
+++ api/apiDataTypes/responseTypes.go
@@ -7,8 +7,27 @@ type User struct {
 }
 
 type ResponseLogin struct {
-  User User `json:"user"`
+	User User `json:"user"`
 }
 type ResponseSignup struct {
-  User User `json:"user"`
+	User User `json:"user"`
+}
+
+type ResponseShortPost struct {
+	ID           int    `json:"id"`
+	Title        string `json:"title"`
+	Description  string `json:"desc"`
+	CreatedAt    string `json:"createdAt"`
+	AuthorName   string `json:"authorName"`
+	AuthorImgURL string `json:"authorImg"`
+}
+
+type ResponseCompletePost struct {
+	ID           int    `json:"id"`
+	Title        string `json:"title"`
+	Content      string `json:"content"`
+	Description  string `json:"desc"`
+	CreatedAt    string `json:"createdAt"`
+	AuthorName   string `json:"authorName"`
+	AuthorImgURL string `json:"authorImg"`
 }
