242cc3a6b460f3072a9a9f481464ed79fb9a1946
JesusJMM <jesusjaviermarcanomora.2005@gmail.com>
2022-06-13 11:58:11 -0400

    payloadTypes updated

--- api/apiDataTypes/payloadTypes.go
+++ api/apiDataTypes/payloadTypes.go
@@ -16,11 +16,17 @@ type PayloadCreatePost struct {
 	Title       string `json:"title" binding:"required"`
 	Description string `json:"desc" binding:"required"`
 	Content     string `json:"content" binding:"required"`
+	Slug        string `json:"slug" binding:"required"`
+	FeedImg     string `json:"feedImg"`
+	ArticleImg  string `json:"articleImg"`
 }
 
 type PayloadUpdatePost struct {
-  ID          int    `json:"id" binding:"required"`
+	ID          int    `json:"id" binding:"required"`
 	Title       string `json:"title"`
 	Description string `json:"desc"`
 	Content     string `json:"content"`
+	Slug        string `json:"slug"`
+	FeedImg     string `json:"feedImg"`
+	ArticleImg  string `json:"articleImg"`
 }
